68a423e12203dc729b1d144f644c78c31d99fb12
Abdullah2213565 <dulakhan024@gmail.com>
2022-09-27 17:13:11 +0200

    Changes required done

--- classes/trimmer_decorator.rb
+++ classes/trimmer_decorator.rb
@@ -3,6 +3,6 @@ require_relative 'base_decorator'
 # This is TrimmerDecorator class
 class TrimmerDecorator < BaseDecorator
   def correct_name
-    @nameable.correct_name[0..10]
+    @nameable.correct_name[0...10]
   end
 end
